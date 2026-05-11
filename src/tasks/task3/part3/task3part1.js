6c5cc672b893534edc9e4652594ddb1b291ee244
AlexandrK <alecsandr-k-18@mail.ru>
2026-05-11 14:41:18 +0300

    adde part 3

---
+++ src/tasks/task3/part3/task3part1.js
@@ -0,0 +1,94 @@
+import Simulation1 from "./simulation1.js";
+import Simulation2 from "./simulation2.js";
+import Simulation3 from "./simulation3.js";
+
+class Timer {
+  #timePoint = 0;
+
+  reset() {
+    this.#timePoint = performance.now();
+  }
+
+  getTime() {
+    const currentTime = performance.now();
+    return currentTime - this.#timePoint;
+  }
+}
+
+const SimulationStates = {
+  STOP: 0,
+  RUN: 1,
+  RESET: 2,
+};
+
+const sketch1 = (p5) => {
+  const simulation = new Simulation1(p5);
+  const timer = new Timer();
+  let state = SimulationStates.RUN;
+
+  p5.setup = () => {
+    p5.createCanvas(400, 400, p5.WEBGL);
+    timer.reset();
+  };
+
+  p5.draw = () => {
+    const dt = timer.getTime();
+    timer.reset();
+
+    if (state == SimulationStates.RUN) {
+      simulation.update(dt);
+    }
+
+    p5.background(200);
+    simulation.draw();
+  };
+};
+new p5(sketch1);
+
+const sketch2 = (p5) => {
+  const simulation = new Simulation2(p5);
+  const timer = new Timer();
+  let state = SimulationStates.RUN;
+
+  p5.setup = () => {
+    p5.createCanvas(400, 400, p5.WEBGL);
+    timer.reset();
+  };
+
+  p5.draw = () => {
+    const dt = timer.getTime();
+    timer.reset();
+
+    if (state == SimulationStates.RUN) {
+      simulation.update(dt);
+    }
+
+    p5.background(200);
+    simulation.draw();
+  };
+};
+new p5(sketch2);
+
+const sketch3 = (p5) => {
+  const simulation = new Simulation3(p5);
+  const timer = new Timer();
+  let state = SimulationStates.RUN;
+
+  p5.setup = () => {
+    p5.createCanvas(400, 400, p5.WEBGL);
+    timer.reset();
+  };
+
+  p5.draw = () => {
+    const dt = timer.getTime();
+    timer.reset();
+
+    if (state == SimulationStates.RUN) {
+      simulation.update(dt);
+    }
+
+    p5.background(200);
+    simulation.draw();
+  };
+};
+new p5(sketch3);
